1ddf9b54c9605c72e3bb701d91b0c37ccc5fd239
sabaebrahimi <saba.eb18@gmail.com>
2021-03-19 08:54:42 +0330

    set menu

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, Menu, ipcMain } = require('electron');
 const path = require('path');
 
-process.env.NODE_ENV = 'production';
+// process.env.NODE_ENV = 'production';
 
 if (require('electron-squirrel-startup')) {
   app.quit();
 }
-let mainWindow;
+let mainWindow, setBluetoothWindow;
 const createWindow = () => {
   mainWindow = new BrowserWindow({
     width: 1370,
@@ -22,6 +22,9 @@ const createWindow = () => {
   });
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
   // mainWindow.webContents.openDevTools();
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  Menu.setApplicationMenu(mainMenu);
+
 };
 
 app.on('ready', createWindow);
@@ -38,3 +41,63 @@ app.on('activate', () => {
   }
 });
 
+const createBluetoothSetWindow = () => {
+  setBluetoothWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: 'Set Bluetooth Port',
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  setBluetoothWindow.loadFile(path.join(__dirname, 'setBluetoothWindow.html'));
+};
+
+const mainMenuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Settings',
+        submenu: [
+          {
+            label: 'Set Bluetooth Port',
+            click() {
+              createBluetoothSetWindow();
+            }
+          }
+        ]
+      },
+      {
+        label: 'Quit',
+        accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+
+if (process.platform == 'darwin') {
+  mainMenuTemplate.unshift({});
+}
+if (process.env.NODE_ENV !== 'production') {
+  mainMenuTemplate.push({
+    label: 'Developer Tools',
+    submenu: [
+      {
+        label: 'Toggle DevTools',
+        accelerator: process.platform == 'darwin' ?
+          'Command+I' : 'Ctrl+I',
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        role: 'reload'
+      }
+    ]
+  });
+}
